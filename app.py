ef3becd2a904c8cf9c3b3ecdee9be6212110ad9e
tomcat6000 <boehmz8613@my.uwstout.edu>
2021-05-31 23:22:14 -0500

    Loops finished

--- app.py
+++ app.py
@@ -1,27 +1,6 @@
-command = ""
-previous_command = ""
+for x in range(4):
+	output = ""
+	for y in range(4):
+		output += ' ' + f'({x}, {y})'
+	print(output)
 
-while True:
-	command = input('> ').lower()
-	if command == 'start':
-		if previous_command != "start":
-			print("Car started...")
-			previous_command = "start"
-		else:
-			print("Car is already started...")
-	elif command == 'stop':
-		if previous_command != 'stop':
-			print("Car stopped...")
-			previous_command = command
-		else:
-			print("Car already stopped...")
-	elif command == 'help':
-		print("""
-start - to start the cars
-stop - to stop the cars
-quit - to quit
-		""")
-	elif command == "quit":
-		break
-	else:
-		print("Sorry, I don't understand that!")
